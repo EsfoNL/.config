9daf8804032e3351927e924d693f2c59b9967645
Esra Fokker <esra.fokker@finalist.nl>
2023-04-07 11:36:19 +0200

    initial

---
+++ sxhkd/lock.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+
+i3lock -k -c 00000055 \
+		--inside-color=000000aa --insidever-color=000000aa --insidewrong-color=000000aa \
+		--layout-color=ffffffff --time-color=ffffffff --date-color=ffffffff --greeter-color=ffffffff \
+		--verif-color=ffffffff --wrong-color=ffffffff --modif-color=ffffffff \
+	  	--time-font=FantasqueSansMono\ Nerd\ Font --date-font=FantasqueSansMono\ Nerd\ Font --verif-font=FantasqueSansMono\ Nerd\ Font --wrong-font=FantasqueSansMono\ Nerd\ Font --greeter-font=FantasqueSansMono\ Nerd\ Font \
+		--indicator --line-color=00000000
